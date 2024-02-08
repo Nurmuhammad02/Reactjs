6ae311175d85f078b90c484de01ca42fe9742f51
Nurmuhammad R <nurmuhammad.rn@gmail.com>
2024-02-08 17:20:45 +0500

    components

---
+++ my-app/src/Technologies.js
@@ -0,0 +1,11 @@
+const Technologies = () => {
+    return (
+      <div>
+        <a href="#s">Css</a>
+        <a href="#s">HTML</a>
+        <a href="#s">Javascript</a>
+      </div>
+    );
+  }
+
+  export default Technologies;

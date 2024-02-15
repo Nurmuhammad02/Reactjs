15f6478d098f2f4bbcf4c99003595fea21b833f8
Nurmuhammad R <nurmuhammad.rn@gmail.com>
2024-02-15 17:08:30 +0500

    redux used

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import store from './redux/state';
+import store from './redux/store';
 import { BrowserRouter } from 'react-router-dom';
 
 
@@ -19,5 +19,8 @@ let rerenderEntireTree = (state) => {
 
 rerenderEntireTree(store.getState());
 
-store.subscribe(rerenderEntireTree)
+store.subscribe(() => {
+    let state = store.getState();
+    rerenderEntireTree(state);
+})
 

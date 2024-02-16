e95051665b911bfe0f03569ff131827aeac06731
Nurmuhammad R <nurmuhammad.rn@gmail.com>
2024-02-16 15:08:30 +0500

    exercise with redux decomposition

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
-import Dialogs from './components/Dialogs/Dialogs';
+import DialogsContainer from './components/Dialogs/DialogsContainer';
 import { Route, Routes } from 'react-router-dom';
 
 const App = (props) => {
@@ -13,8 +13,8 @@ const App = (props) => {
         <Navbar />
         <div className='app-wrapper-content'>
           <Routes>
-            <Route path='/dialogs' element={<Dialogs store={props.store} />} />
-            <Route path='/profile' element={<Profile profilePage={props.state.profilePage} dispatch={props.dispatch}/>} />
+            <Route path='/dialogs' element={<DialogsContainer store={props.store} />} />
+            <Route path='/profile' element={<Profile store={props.store}/>} />
           </Routes>
         </div>
       </div>
--- my-app/src/components/Profile/MyPosts/MyPosts.jsx
+++ my-app/src/components/Profile/MyPosts/MyPosts.jsx
@@ -8,14 +8,15 @@ const MyPosts = (props) => {
 
     let newPostElement = React.createRef();
 
-    let addPost = () => {
-        props.dispatch(addPostActionCreator())
+    let onAddPost = () => {
+        props.addPost();
     }
-
+    
     let onPostChange = () => {
         let text = newPostElement.current.value;
-        let action = updateNewPostTextActionCreator(text);
-        props.dispatch(action);
+        props.updateNewPostText(text);
+        // let action = updateNewPostTextActionCreator(text);
+        // props.dispatch(action);
     }
 
     return <div className={s.postsBlock}>
@@ -25,7 +26,7 @@ const MyPosts = (props) => {
                 <textarea ref={newPostElement} value={props.newPostText} onChange={onPostChange} />
             </div>
             <div>
-                <button onClick={addPost}>Add post</button>
+                <button onClick={onAddPost}>Add post</button>
             </div>
         </div>
         <div className={s.posts}>

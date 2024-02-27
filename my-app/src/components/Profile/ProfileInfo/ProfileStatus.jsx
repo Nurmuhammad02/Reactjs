bbb44c9f5e71d558b48d53db4a2a76ffe3cb6a70
Nurmuhammad02 <nurmuhammad.rn@gmail.com>
2024-02-27 11:15:46 +0500

    Update

--- my-app/src/components/Profile/ProfileInfo/ProfileStatus.jsx
+++ my-app/src/components/Profile/ProfileInfo/ProfileStatus.jsx
@@ -3,12 +3,11 @@ import s from './ProfileInfo.module.css';
 import Preloader from '../../Common/Preloader/Preloader';
 
 class ProfileStatus extends React.Component {
-  
     state = {
         editMode: false,
         status: this.props.status
     };
-
+    
     activeEditMode = () => {
         this.setState({ editMode: true });
     };
@@ -16,12 +15,19 @@ class ProfileStatus extends React.Component {
         this.setState({ editMode: false });
         this.props.updateStatus(this.state.status);
     };
-
+    
     onStatusChange = (e) => {
         this.setState({
             status: e.currentTarget.value
         })
-
+        
+    }
+    componentDidUpdate(prevProps, prevState) {
+        if(prevProps.status !== this.props.status) {
+            this.setState({
+                status: this.props.status
+            })
+        }
     }
 
     render() {

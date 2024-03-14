14616da703ec711ce1d557fdc746e02732a573a0
Nurmuhammad02 <nurmuhammad.rn@gmail.com>
2024-03-14 09:58:18 +0500

    some styping

---
+++ my-app/src/components/Profile/MyPosts/MyPostsContainer.tsx
@@ -0,0 +1,34 @@
+import { addPostActionCreator } from '../../../redux/profile-reducer';
+import MyPosts from './MyPosts';
+import { connect } from 'react-redux';
+import {AppStateType} from "../../../redux/redux-store.ts";
+import {PostsType} from "../../../Types/types.ts";
+
+type MapStateToPropsType = {
+    posts: PostsType[]
+    newPostsText: string
+}
+
+const mapStateToProps = (state: AppStateType): MapStateToPropsType => {
+    return {
+        posts: state.profilePage.posts,
+        newPostsText: state.profilePage.newPostsText,
+    }
+}
+
+type MapDispatchToPropsType = {
+    addPost: (text:string) => void
+}
+
+const mapDispatchToProps = (dispatch: any): MapDispatchToPropsType => {
+    return {
+        addPost: (text: string) => {
+            dispatch(addPostActionCreator(text));
+        },
+    }
+}
+
+//@ts-ignore
+const MyPostsContainer = connect<MapStateToPropsType,MapDispatchToPropsType>(mapStateToProps, mapDispatchToProps)(MyPosts);
+
+export default MyPostsContainer;

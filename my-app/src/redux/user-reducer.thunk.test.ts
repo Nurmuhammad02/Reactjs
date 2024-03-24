6c80df71d0794ce3795f2552ef31e1cc79444f62
Nurmuhammad02 <nurmuhammad.rn@gmail.com>
2024-03-24 13:38:40 +0500

    unit test incompletely user-reducer

--- my-app/src/redux/user-reducer.thunk.test.ts
+++ my-app/src/redux/user-reducer.thunk.test.ts
@@ -1,9 +1,20 @@
-import {actions, follow} from "./users-reducer.ts";
+import {actions, follow, unfollow} from "./users-reducer.ts";
 import {usersApi} from "../components/api/users-api.ts";
 import {ResponseType, ResultCodesEnum} from "../components/api/api.ts";
 
 jest.mock("../components/api/users-api.ts")
 const userApiMock = usersApi as jest.Mocked<typeof usersApi>;
+
+const dispatchMock = jest.fn();
+const getStateMock = jest.fn();
+
+beforeEach(() => {
+    dispatchMock.mockClear();
+    getStateMock.mockClear();
+    userApiMock.follow.mockClear();
+    userApiMock.unfollow.mockClear();
+
+})
 
 const result: ResponseType = {
     resultCode: ResultCodesEnum.Success,
@@ -15,15 +26,15 @@ const result: ResponseType = {
 // After the mock return value is set up
 userApiMock.follow.mockReturnValue(Promise.resolve(result));
 
-test("", () => {
-    const thunk = follow(1)
-    const dispatchMock = jest.fn()
+test("",  async () => {
+    const thunk = unfollow(1);
 
-    // @ts-ignore
-    thunk(dispatchMock)
+
+
+    await thunk(dispatchMock, getStateMock, {})
 
     expect(dispatchMock).toBeCalledTimes(1)
     expect(dispatchMock).toHaveBeenCalledWith(1, actions.toggleFollowingProgress(true, 1))
-    expect(dispatchMock).toHaveBeenCalledWith(2, actions.succesFollow(1))
+    expect(dispatchMock).toHaveBeenCalledWith(2, actions.succesUnfollow(1))
     expect(dispatchMock).toHaveBeenCalledWith(3, actions.toggleFollowingProgress(false, 1))
 })
